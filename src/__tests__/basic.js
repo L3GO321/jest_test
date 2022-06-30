06e89dab3d8997a1596b3cfffdb4365c9905d297
L3GO321 <googlik701@gmail.com>
2022-06-30 14:02:54 +0300

    appveyour fix

--- src/__tests__/basic.js
+++ src/__tests__/basic.js
@@ -1,23 +1,20 @@
-import { game } from '../app.js';
+import game from '../app';
 
 test('testPers', () => {
-    const list =
-    {
-        name: 'Маг',
-        health: 90,
-    };
-    const list2 =
-    {
-        name: 'Маг',
-        health: 45,
-    };
-    const list3 =
-    {
-        name: 'Маг',
-        health: 14,
-    };
+  const list = {
+    name: 'Маг',
+    health: 90,
+  };
+  const list2 = {
+    name: 'Маг',
+    health: 45,
+  };
+  const list3 = {
+    name: 'Маг',
+    health: 14,
+  };
 
-    expect(game(list)).toBe('healthy');
-    expect(game(list2)).toBe('wounded');
-    expect(game(list3)).toBe('critical');
-}); 
+  expect(game(list)).toBe('healthy');
+  expect(game(list2)).toBe('wounded');
+  expect(game(list3)).toBe('critical');
+});
